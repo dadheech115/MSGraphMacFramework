96f3be8febbf9f739dd3dc1544d74bee010e3bf1
Vikas Dadheech <vidadhee@microsoft.com>
2018-07-09 13:12:04 +0530

    Initial Commit

---
+++ MSGraphMacFramework/MSGraphMacFramework.h
@@ -0,0 +1,19 @@
+//
+//  MSGraphMacFramework.h
+//  MSGraphMacFramework
+//
+//  Created by Vikas Dadheech on 09/07/18.
+//  Copyright © 2018 Vikas Dadheech. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for MSGraphMacFramework.
+FOUNDATION_EXPORT double MSGraphMacFrameworkVersionNumber;
+
+//! Project version string for MSGraphMacFramework.
+FOUNDATION_EXPORT const unsigned char MSGraphMacFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MSGraphMacFramework/PublicHeader.h>
+
+
